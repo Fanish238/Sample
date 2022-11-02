35502958c9acdea40f76a6d23ad27f746efe5a52
Михаил <konovalchik1239@mail.ru>
2022-11-03 01:29:07 +0300

    Убейте меня xxd

--- app/src/main/java/com/example/mpt7/MainActivity.java
+++ app/src/main/java/com/example/mpt7/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.mpt7;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -10,35 +12,45 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView one, two, three, four;
+    ImageView one, two, three, four, oApk;
+    AlertDialog.Builder dialog;
 
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        one = findViewById(R.id.one);
-        two = findViewById(R.id.two);
-        three = findViewById(R.id.three);
-        four = findViewById(R.id.four);
+        one = findViewById(R.id.one_img);
+        two = findViewById(R.id.two_img);
+        three = findViewById(R.id.three_img);
+        four = findViewById(R.id.four_img);
+        oApk = findViewById(R.id.background_oApk);
     }
 
     @SuppressLint("NonConstantResourceId")
     public void List_activity(View view) {
         switch (view.getId()) {
-            case R.id.one:
+            case R.id.one_background:
                 Trans(1);
                 break;
-            case R.id.two:
+            case R.id.two_background:
                 Trans(2);
                 break;
-            case R.id.three:
+            case R.id.three_background:
                 Trans(3);
                 break;
-            case R.id.four:
+            case R.id.four_background:
                 Trans(4);
                 break;
         }
+    }
+
+    public void oApk_OnClick(View view) {
+        dialog = new AlertDialog.Builder(MainActivity.this);
+        dialog.setTitle("О приложении");
+        dialog.setMessage(R.string.oApk_string);
+        dialog.setPositiveButton("Закрыть", (dialog, i) -> dialog.cancel());
+        dialog.show();
     }
 
     public void Trans(int a) {
