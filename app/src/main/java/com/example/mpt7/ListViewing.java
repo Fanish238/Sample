7f2e25ae5354b8903440ded079b77bcf12aa9fd1
Михаил <konovalchik1239@mail.ru>
2022-11-09 01:42:45 +0300

    Перенос данных

--- app/src/main/java/com/example/mpt7/ListViewing.java
+++ app/src/main/java/com/example/mpt7/ListViewing.java
@@ -1,6 +1,5 @@
 package com.example.mpt7;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
@@ -14,17 +13,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ListViewing extends AppCompatActivity {
-    TextView textView;
+    TextView model_1,model_2,model_3,model_4,model_5,model_6,model_7,model_8;
     ImageView cell_1,cell_2,cell_3,cell_4,cell_5,cell_6,cell_7,cell_8;
-    List<ImageView> list = new ArrayList<>();
+    List<ImageView> list_img = new ArrayList<>();
+    List<TextView> list_text = new ArrayList<>();
     Dialog dialog;
+    List<String> name = new ArrayList<>();
+    String[] name_mas_one,name_mas_two;
+    String mDrawableName;
+    int resDrawableID;
 
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
-        textView = findViewById(R.id.textView1);
+        model_1 = findViewById(R.id.textView1);
+        model_2 = findViewById(R.id.textView2);
+        model_3 = findViewById(R.id.textView3);
+        model_4 = findViewById(R.id.textView4);
+        model_5 = findViewById(R.id.textView5);
+        model_6 = findViewById(R.id.textView6);
+        model_7 = findViewById(R.id.textView7);
+        model_8 = findViewById(R.id.textView8);
+
         cell_1 = findViewById(R.id.imageView1);
         cell_2 = findViewById(R.id.imageView2);
         cell_3 = findViewById(R.id.imageView3);
@@ -34,19 +46,38 @@ public class ListViewing extends AppCompatActivity {
         cell_7 = findViewById(R.id.imageView7);
         cell_8 = findViewById(R.id.imageView8);
 
-        list.add(cell_1);
-        list.add(cell_1);
-        list.add(cell_2);
-        list.add(cell_3);
-        list.add(cell_4);
-        list.add(cell_5);
-        list.add(cell_6);
-        list.add(cell_7);
-        list.add(cell_8);
+        name_mas_one = new String[]{null,"Volkswagen Corrado","Volkswagen Eos","Volkswagen Scirocco I",
+        "Volkswagen Scirocco II","Volkswagen Arteon","Volkswagen Passat CC","Volkswagen Passat СС, I поколение, рестайлинг","Volkswagen Beetle"};
+        name_mas_two = new String[]{null,"Volkswagen Passat B1","Volkswagen Passat B2","Volkswagen Passat B3","Volkswagen Passat B4",
+        "Volkswagen Passat B5", "Volkswagen Passat B6","Volkswagen Passat B7"};
 
-        textView.setText(""+getIntent().getIntExtra("a",0));
-        for (int i = 1; i<=list.size()-1 ;i++){
-            list.get(i).setImageResource(R.drawable.passat_b3);
+        list_img.add(cell_1);
+        list_img.add(cell_1);
+        list_img.add(cell_2);
+        list_img.add(cell_3);
+        list_img.add(cell_4);
+        list_img.add(cell_5);
+        list_img.add(cell_6);
+        list_img.add(cell_7);
+        list_img.add(cell_8);
+
+        list_text.add(model_1);
+        list_text.add(model_1);
+        list_text.add(model_2);
+        list_text.add(model_3);
+        list_text.add(model_4);
+        list_text.add(model_5);
+        list_text.add(model_6);
+        list_text.add(model_7);
+        list_text.add(model_8);
+
+        for(int i = 1; i< list_text.size()-1; i++){
+            list_text.get(i).setText(name_mas_one[i]);
+        }
+        for (int i = 1; i<= list_img.size()-1 ; i++){
+            mDrawableName = "_1_"+i;
+            resDrawableID = getResources().getIdentifier(mDrawableName, "drawable", getPackageName());
+            list_img.get(i).setImageResource(resDrawableID);
         }
     }
 
